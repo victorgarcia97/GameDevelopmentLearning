200192e507864e38c5b9d236027661a8a7b4edc6
Victor Garcia <vgg608@alumnos.unican.es>
2019-11-04 08:55:09 +0100

    Comienzo

---
+++ Pruebas/Assets/Move.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizantalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+
+    public CharacterController player;
+
+    public float speed;
+    private Vector3 movePlayer;
+    public float gravity = 9.8f;
+    public float fallSpeed;
+    public float jumpForce;
+        
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+    
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizantalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        
+        
+        playerInput = new Vector3(horizantalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+        movePlayer = movePlayer * speed;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        setGravity();
+
+        playerSkills();
+
+
+        player.Move(movePlayer * Time.deltaTime);
+        
+    }
+
+    void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    void setGravity()
+    {
+        if(player.isGrounded)
+        {
+            fallSpeed = -gravity * Time.deltaTime;
+            movePlayer.y = fallSpeed;
+        }
+        else
+        {
+            fallSpeed -= gravity * Time.deltaTime;
+            movePlayer.y = fallSpeed;
+        }
+    }
+
+    void playerSkills()
+    {
+        if(player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallSpeed = jumpForce;
+            movePlayer.y = fallSpeed;
+        }
+    }
+}
---
+++ Pruebas/Assets/movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class movement : MonoBehaviour
+{
+    private const int Y = 2;
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody rb;
+    public bool isGrounded;//Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, Y, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
